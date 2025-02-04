6fd710d5267f00be00ac3fd7bd00217c4df61c6b
A477752 <kani.abidhanya@gmail.com>
2025-02-05 00:38:30 +0530

    Add files via upload

--- appcode.py
+++ appcode.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1eeu-NdfwnZt2VpvuqiAIZUtQ9-an_-g0
 """
 
-pip install streamlit
-
 import streamlit as st
 import tensorflow as tf
 import numpy as np
